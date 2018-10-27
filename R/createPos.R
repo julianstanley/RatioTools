d006ddae2701524094027706a54d4006cbe0bcf7
julianstanley <stanley.ju@husky.neu.edu>
2018-10-27 13:16:30 -0400

    Added documentation to createPos.R

---
+++ R/createPos.R
@@ -0,0 +1,14 @@
+#' Create a 2-column matrix from two vectors
+#' @param X one array of length n
+#' @param Y another array of length n
+#' @examples
+#' createPos(c(1,2,3), c(1,4,9))
+#' @export
+
+# Helper to create a 2-column matrix from two vectors
+createPos <- function(X, Y) {
+  
+  return( 
+    cbind(matrix(na.omit(X)), na.omit(Y))
+  );
+}
