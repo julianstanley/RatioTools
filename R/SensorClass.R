a2c58aeef93fe3cd776e74179c2cdc83eb66a7f1
julianstanley <stanley.ju@husky.neu.edu>
2018-10-27 21:38:34 -0400

    Documented more, passes tests

--- R/SensorClass.R
+++ R/SensorClass.R
@@ -1,5 +1,4 @@
 #' An S4 class to represent a sensor
-#'
 #' @slot ox A 2xN numeric matrix, 
 #' representing [x, y] of sensor in oxidized state
 #' @slot red A 2xN numeric matrix, 
@@ -11,16 +10,11 @@ setClass("Sensor",
                 red = "matrix", E0 = "numeric"))
 
 # ----------- Sensor constructors
-# Main constructor
-# ARGS: Two, 2-by-N matricies 
-# -- oxMatrix: matrix of x and y values in oxidized state
-# -- redMatrix: matrix of x and y values in reduced state
-
-
 #' A function to construct a Sensor class based on two matricies
 #' @param oxMatrix A 2XN numeric matrix
 #' @param redMatrix A 2xN numeric matrix
 #' @param E0 a numeric value for the midpoint potential of the sensor
+#' @return A sensor object
 Sensor <- function(oxMatrix, redMatrix, E0) {
     return(new("Sensor", ox = oxMatrix,
         red = redMatrix, E0 = E0));
@@ -33,13 +27,25 @@ Sensor <- function(oxMatrix, redMatrix, E0) {
 #' at the wavelengths of xOx
 #' @param xRed A numeric array representing wavelength 
 #' values in a reduced state
+#' @param yRed A numeric array representing relative absorption
+#' at the wavelenghts of xRed
 #' @param E0 a numeric value for the midpoint potential of the sensor
+#' @return A sensor object
 SensorPoint <- function(xOx, yOx, xRed, yRed, E0) {
     return(Sensor(createPos(xOx, yOx), createPos(xRed, yRed), E0));
 }
 
-# Additional constructor for Rmax, Rmin, and delta
-# Helps by making vectors corresponding to values given
+#' A function to construct a sensor class based on sensor properties
+#' @param Rmax the maximum ratiometric intensity value
+#' @param Rmin the minimum ratiometric intensity value
+#' @param delta the dynamic range of the divisor wavelength
+#' @param lambda_1 (optional) dividend wavelength, default = 410
+#' @param lambda_2 (optional) divisor wavelength, default = 470
+#' @param E0 (optional) midpoint potential of the sensor, 
+#' default=-265
+#' @usage SensorValues(Rmax, Rmin, delta,
+#' lambda_1 = 410, lambda_2 = 470, E0 = -265)
+#' @return A sensor object
 SensorValues <- function(Rmax, Rmin, delta, 
                         lambda_1 = 410, lambda_2 = 470, E0 = -265) {
     return(Sensor(createPos(c(lambda_1, lambda_2),c(Rmax*delta, delta)), 
--- tests/testthat/testCreatePos.R
+++ tests/testthat/testCreatePos.R
@@ -1,6 +1,22 @@
+context("testCreatePos.r")
+
 library(RatioTools)
 
 test_that("createPos returns a matrix with two columns", {
 		  expect_equal(createPos(c(1,2), c(1,4)), cbind(matrix(c(1,2)), c(1,4)))
 		  expect_equal(dim(createPos(c(1,2), c(1,4)))[2], 2)
 })
+
+test_that("createPos errors when given empty vectors", {
+		  # Warning should be called by remove.na from a null vector
+		  expect_warning(expect_error(createPos(c(),c())))
+})
+
+test_that("createPos removes NULL values from vectors", {
+		  expect_equal(createPos(c(1,2,NA), c(1,NA,4)), 
+			       cbind(matrix(c(1,2)), c(1,4)))
+})
+
+
+
+
