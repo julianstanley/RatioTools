85e69bb1a51f1aa9e1b2f331490310bf7d972bbc
julianstanley <stanley.ju@husky.neu.edu>
2018-10-27 20:04:53 -0400

    Expanded docs and tests

--- R/SensorClass.R
+++ R/SensorClass.R
@@ -1,8 +1,8 @@
 #' An S4 class to represent a sensor
 #'
-#' @slot ox A 2x2 numeric matrix, 
+#' @slot ox A 2xN numeric matrix, 
 #' representing [x, y] of sensor in oxidized state
-#' @slot red A 2x2 numeric matrix, 
+#' @slot red A 2xN numeric matrix, 
 #' representing [x, y] of sensor in reduced state
 #' @slot E0 a numeric value for the midpoint potential of the sensor
 #' @importFrom methods new
@@ -15,12 +15,25 @@ setClass("Sensor",
 # ARGS: Two, 2-by-N matricies 
 # -- oxMatrix: matrix of x and y values in oxidized state
 # -- redMatrix: matrix of x and y values in reduced state
+
+
+#' A function to construct a Sensor class based on two matricies
+#' @param oxMatrix A 2XN numeric matrix
+#' @param redMatrix A 2xN numeric matrix
+#' @param E0 a numeric value for the midpoint potential of the sensor
 Sensor <- function(oxMatrix, redMatrix, E0) {
     return(new("Sensor", ox = oxMatrix,
         red = redMatrix, E0 = E0));
 }
 
-# Additional constructor for seperate x and y arrays
+#' An additional function-constructor for seperate x and y arrays
+#' @param xOx A numeric array representing wavelength 
+#' values in an oxidized state
+#' @param yOx A numeric array representing relative absorption 
+#' at the wavelengths of xOx
+#' @param xRed A numeric array representing wavelength 
+#' values in a reduced state
+#' @param E0 a numeric value for the midpoint potential of the sensor
 SensorPoint <- function(xOx, yOx, xRed, yRed, E0) {
     return(Sensor(createPos(xOx, yOx), createPos(xRed, yRed), E0));
 }
